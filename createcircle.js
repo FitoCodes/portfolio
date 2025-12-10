d0ba968773c19689ceb62c5c280d68d361d0e002
FitoCodes <brookshmoreland@gmail.com>
2025-12-09 19:18:36 -0800

    12/9

--- createcircle.js
+++ createcircle.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 
-    const mouseX = event.clientX -50;
-    const mouseY = event.clientY -50;
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
     const size = document.getElementById("input").value
 
 
@@ -26,9 +26,28 @@ document.addEventListener('DOMContentLoaded', () => {
     circle.style.backgroundColor = randomColor;
     backgroundContainer.appendChild(circle)
 
+    console.log(`X-coordinate: ${mouseX}, Y-coordinate: ${mouseY}`);
+
+    const line = document.createElement('div')
+    line.classList.add("diagonal-line");  
+    line.style.height = 100
+    line.style.width = 2
+    line.style.transform
+
+
+    //width: 200px; 
+  //height: 2px; 
+  //background-color: black;
+  //transform: rotate(45deg);
+  //transform-origin: 90 90;
+  //https://docs.google.com/document/d/1ukq88CqcKbPi9wm9_I9oVzYtb-LRcbHuY_kyX6cKCcM/edit?tab=t.0
+
+
+  
+    })
 })
 
-})
+
 
 
 
